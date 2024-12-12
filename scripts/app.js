34658c15c2f75162c33dde1a6f8759147c011006
herman <hermandaza98@gmail.com>
2024-12-12 16:55:50 -0500

    Actualizacion_04

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,14 @@
 const projects = [
     {
-
+        idProject:1,
+        img-Project: 'https://res.cloudinary.com/dfrw0yyhb/image/upload/v1732135644/FB_IMG_1694014577707_gjingx.jpg'
+        titleproject: 'Pagina Web'
+        descriptionProject: 'Una pagina web donde muestro mis albumnes favoritos en torno a la musica noise'
+        linkProject:''
     }
     {
         idProject:2,
-        img-Project: 'https://res.cloudinary.com/dfrw0yyhb/image/upload/v1732135644/FB_IMG_1694014577707_gjingx.jpg'
+        imgProject: 'https://res.cloudinary.com/dfrw0yyhb/image/upload/v1732135644/FB_IMG_1694014577707_gjingx.jpg'
         titleproject: 'Pagina Web'
         descriptionProject: 'Una pagina web donde muestro mis albumnes favoritos en torno a la musica noise'
         linkProject:''
@@ -16,121 +20,104 @@ const projects = [
         descriptionProject: 'Una pagina web donde muestro mis albumnes favoritos en torno a la musica noise'
         linkProject:''
     }
-    {
-        idProject:4,
-        imgProject: 'https://res.cloudinary.com/dfrw0yyhb/image/upload/v1732135644/FB_IMG_1694014577707_gjingx.jpg'
-        titleproject: 'Pagina Web'
-        descriptionProject: 'Una pagina web donde muestro mis albumnes favoritos en torno a la musica noise'
-        linkProject:''
-    }
-    {
-        idProject:1,
-        img-Project: 'https://res.cloudinary.com/dfrw0yyhb/image/upload/v1732135644/FB_IMG_1694014577707_gjingx.jpg'
-        title-project: 'Pagina Web'
-        description-Project: 'Una pagina web donde muestro mis albumnes favoritos en torno a la musica noise'
-        link-Project:''
-    }
 ]
 
 const references = [
     {
-        nombre: "Coromoto Manzano"
-        cargo: "Contratista"
-        telefono: "301368696"
-        correo: "angls90@outlook.es"
+        name: "Coromoto Manzano"
+        ocupation: "Contratista"
+        phone: "301368696"
+        email: "angls90@outlook.es"
         idreferences: '1'
     }
     {
-        nombre: "James Tautiva"
-        cargo: "Desarrollador Web"
-        telefono: "3229187896"
-        correo: "jamesta@outlook.es"
+        name: "James Tautiva"
+        ocupation: "Desarrollador Web"
+        phone: "3229187896"
+        email: "jamesta@outlook.es"
         idreferences: '2'
     }
     {
         nombre: "Oscar Muñoz"
-        cargo: "Asistente medico"
-        telefono: "3142867919"
-        correo: "muñososc@outlook.es"
+        cocupation: "Asistente medico"
+        phone: "3142867919"
+        email: "muñososc@outlook.es"
         idreferences: '3'
     }
 ]
 
 const experience = [
     {
-        nombreLenguaje: "java",
-        progresoPorcentaje: 40,
-        imagen: ""
+        langueageName: "java",
+        percentprogress: 40,
+        image: ""
     }
     {
-        nombreLenguaje: "react"
-        progresoPorcentaje: 40,
-        imagen: ""
+        langueageName: "Css",
+        percentprogress: 40,
+        image: ""
     }
     {
-        nombreLenguaje: "html"
-        progresoPorcentaje: 40,
-        imagen: ""
+        langueageName: "Html",
+        percentprogress: 40,
+        image: ""
     }
-    {
-        nombreLenguaje: "CSS"
-        progresoPorcentaje: 40,
-        imagen: ""
-    }
+
 ]
 
-document.qwertyselectorAll('a[hrefʌ="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function(event) {
+document.querySelectorAll('a[href^="#"]').forEach( anchor => {
+    anchor.addEventListener('click', function (event) {
         event.preventDefault();
+        const target = document.querySelector(event.currentTarget('href'));
 
-        const target = document.quertySelector(this.getAttribute('href'))
-
-        if (target) {
+        if(target) {
             window.scrollTo({
                 top: target.offsetTop,
                 behavior: 'smooth'
-            })
+            });
         }
     });
 });
 
-loadSectionproyects()
-function loadsectionProyects(){
-    projects.forEach(project => createcardprojects(project))
+loadSectionProyects()
+function loadSectionProyects(){
+    projects.forEach(proyect=> createCardsProyects(proyect))
 }
-function createcardprojects(project) {
-const cardproject = document.createElement('div')
-cardproject.classList.add('card-proyects')
+function createCardsProyects(proyect) {
+    const cardProyect = document.createElement('div')
+    cardProyect.classList.add('card-proyects')
 
-const containerImg = document.createElement('div')
-containerImg.classList.add('container-img-card')
+    const containerImg = document.createElement('div')
+    containerImg.classList.add('container-img-card')
 
-const imgcard = document.createElement('img')
-imgcard.src = project.imgProject
-imgcard.alt = proyect.titleproject
+    const imgCard = document.createElement('img')
+    imgCard.src = proyect.imgProject
+    imgCard.alt = proyect.titleproject
 
-const containerDescription = document.createElement('div')
-containerDescription.classList.add('container-description-card')
+    const containerdescription = document.createElement('div')
+    containerdescription.classList.add('container-description-card')
 
-const titleCard = document.createElement('h3')
-titleCard.textContent = project.titleproject
+    const titlecard = document.createElement('dh3')
+    titlecard.textContent = proyect.titleproject
 
-const descriptionCard = document.createElement('p')
-descriptionCard.textContent = project.descriptionProject
+    const descriptionCard = document.createElement('p')
+    descriptionCard.textContent = proyect.descriptionProject
 
-const gotoproyect = document.createElement('a')
-gotoproyect.href = project.linkProject
+    const goToproyect = document.createElement('a')
+    goToproyect.href = proyect.linkProject
 
-gotoproyect.setAttribute('target', '_blank')
-gotoproyect.textContent = 'Ir al Proyecto'
+    goToproyect.setAttribute('target','_blank')
+    goToproyect.textContent = 'Ir al Proyecto'
 
-cardproject.appendChild(containerimg)
-cardproject.appendChild(containerDescription)
 
-containerImg.appendChild(imgcard)
-containerDescription.appendChild(titleCard)
-containerDescription.appendChild(descriptionCard)
-containerDescription.appendChild(gotoproyect)
+    cardProyect.appendChild(containerImg)
+    cardProyect.appendChild(containerdescription)
 
-document.queryselector('.container-cards').appendChild(cardproject)
+    cardProyect.appendChild(imgCard)
+    containerdescription.appendChild(titlecard)
+    containerdescription.appendChild(descriptionCard)
+    containerdescription.appendChild(goToproyect)
+
+    document.querySelector('container-cards').appendChild(cardProyect)
+
 }
